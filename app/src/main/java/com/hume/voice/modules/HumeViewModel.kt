c8a0b3eb639a4ea902d5f6967247af26c9aaaec0
weiraneve <907221539@qq.com>
2025-01-07 11:25:36 +0800

    feat: add assistant message toast

--- app/src/main/java/com/hume/voice/modules/HumeViewModel.kt
+++ app/src/main/java/com/hume/voice/modules/HumeViewModel.kt
@@ -1,6 +1,10 @@
 package com.hume.voice.modules
 
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hume.voice.common.obj.Constants
@@ -10,7 +14,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class HumeViewModel : ViewModel() {
+class HumeViewModel(application: Application) : ViewModel() {
 
     companion object {
         private const val TAG = "HumeViewModel"
@@ -22,6 +26,10 @@ class HumeViewModel : ViewModel() {
     private val _messages = MutableStateFlow<List<HumeMessage>>(emptyList())
     val messages: StateFlow<List<HumeMessage>> = _messages.asStateFlow()
 
+    @SuppressLint("StaticFieldLeak")
+    private val context: Context = application.applicationContext
+
+
     fun handleWebSocketMessage(message: HumeMessage) {
         viewModelScope.launch {
             when (message.type) {
@@ -30,6 +38,7 @@ class HumeViewModel : ViewModel() {
 
                     if (message.message.content.isNotEmpty()) {
                         Log.d(TAG, message.message.content)
+                        Toast.makeText(context, message.message.content, Toast.LENGTH_SHORT).show()
                     }
                 }
 
